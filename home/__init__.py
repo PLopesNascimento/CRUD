76e03f949e3b72b245ecce6ad0993a46ad135887
paulo <paulo.loppes@yahoo.com>
2022-01-05 17:18:28 -0300

    exluindo BD

--- home/__init__.py
+++ home/__init__.py
@@ -1,14 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
-from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 
 from home.controller import default
--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import os
 
 if __name__ == "__main__":
    app.run(debug=True)
-   port = int(os.getenv('PORT'), '5000')
-   app.run(host='0.0.0.0', port=port)
+
 
 
