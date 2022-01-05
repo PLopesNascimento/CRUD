3fc0e3f51340d33dfb43191393f6159665ebe03e
paulo <paulo.loppes@yahoo.com>
2022-01-05 17:57:33 -0300

    Testando Servidor

--- home/controller/default.py
+++ home/controller/default.py
@@ -4,4 +4,7 @@ from flask import Flask, render_template
 
 @app.route("/")
 def index():
+    return render_template("curriculo.html")
+@app.route("/login")
+def login():
     return render_template("login.html")
